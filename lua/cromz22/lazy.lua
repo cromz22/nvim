cac320c018f6326c1c0b9146efb599353d2671ad
cromz22 <tt.pfr4343@gmail.com>
2023-09-27 20:54:38 +0900

    change neo-tree configurations

--- lua/cromz22/lazy.lua
+++ lua/cromz22/lazy.lua
@@ -32,7 +32,25 @@ require("lazy").setup({
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
 			"MunifTanjim/nui.nvim",
-		}
+		},
+		config = function()
+			require("neo-tree").setup({
+				close_if_last_window = true,
+				window = {
+					position = "left",
+					width = 30,
+					mappings = {
+						["s"] = "open_split",
+						["v"] = "open_vsplit"
+					},
+				},
+				filesystem = {
+					filtered_items = {
+						visible = true,
+					}
+				}
+			})
+		end,
 	},
 
 	-- fuzzy finder
