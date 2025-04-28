5cd97f727a673779c5825eb8ebf0c9fd278ec5cd
cromz22 <tt.pfr4343@gmail.com>
2025-04-28 18:30:25 +0900

    restrict copilot filetypes

---
+++ after/plugin/copilot.lua
@@ -0,0 +1,9 @@
+vim.g.copilot_filetypes = {
+    ['*'] = false,
+    ['javascript'] = true,
+    ['typescript'] = true,
+    ['python'] = true,
+    ['html'] = true,
+    ['css'] = true,
+    ['lua'] = true,
+}
