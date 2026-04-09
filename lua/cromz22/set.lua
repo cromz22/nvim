eb37b3f07d6ed7fbad8129f822eaaaa2d6acc609
cromz22 <shuichiro.shimizu@keio.jp>
2026-04-09 14:16:36 +0900

    update clipboard config when xclip is available

--- lua/cromz22/set.lua
+++ lua/cromz22/set.lua
@@ -5,7 +5,25 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 0
 vim.opt.expandtab = true
 vim.opt.softtabstop = -1
-vim.opt.clipboard = "unnamed"
+vim.opt.clipboard = "unnamedplus"
+
+if vim.env.SSH_TTY or vim.env.SSH_CONNECTION then
+    vim.g.clipboard = "osc52"
+elseif vim.env.TMUX and vim.fn.executable("xclip") == 1 then
+    vim.g.clipboard = {
+        name = "xclip",
+        copy = {
+            ["+"] = "xclip -selection clipboard -in",
+            ["*"] = "xclip -selection primary -in",
+        },
+        paste = {
+            ["+"] = "xclip -selection clipboard -out",
+            ["*"] = "xclip -selection primary -out",
+        },
+        cache_enabled = 0,
+    }
+end
+
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
